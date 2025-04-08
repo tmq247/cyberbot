ab4becc71d87047ba8a1b38bc3d1dc419432cb18
tmq247 <60788746+tmq247@users.noreply.github.com>
2025-04-09 01:32:37 +0700

    Update admin_misc.py

--- wbb/modules/admin_misc.py
+++ wbb/modules/admin_misc.py
@@ -30,41 +30,41 @@ from wbb.core.decorators.permissions import adminsOnly
 
 __MODULE__ = "Admin Miscs"
 __HELP__ = """
-/set_chat_title - Change The Name Of A Group/Channel.
-/set_chat_photo - Change The PFP Of A Group/Channel.
-/set_user_title - Change The Administrator Title Of An Admin.
+/doitennhom - Thay đổi tên của một nhóm/kênh.
+/doianhnhom - Thay đổi ảnh đại diện của một nhóm/kênh.
+/tenqtv - Thay đổi chức danh quản trị viên của một quản trị viên.
 """
 
 
-@app.on_message(filters.command("set_chat_title") & ~filters.private)
+@app.on_message(filters.command("doitennhom") & ~filters.private)
 @adminsOnly("can_change_info")
 async def set_chat_title(_, message):
     if len(message.command) < 2:
-        return await message.reply_text("**Usage:**\n/set_chat_title NEW NAME")
+        return await message.reply_text("**Cách sử dụng:**\n/doitennhom TÊN MỚI")
     old_title = message.chat.title
     new_title = message.text.split(None, 1)[1]
     await message.chat.set_title(new_title)
     await message.reply_text(
-        f"Successfully Changed Group Title From {old_title} To {new_title}"
+        f"Đã thay đổi thành công tiêu đề nhóm từ {old_title} Thành {new_title}"
     )
 
 
-@app.on_message(filters.command("set_user_title") & ~filters.private)
+@app.on_message(filters.command("tenqtv") & ~filters.private)
 @adminsOnly("can_change_info")
 async def set_user_title(_, message):
     if not message.reply_to_message:
         return await message.reply_text(
-            "Reply to user's message to set his admin title"
+            "Trả lời tin nhắn của người dùng để đặt chức danh quản trị viên của anh ta"
         )
     if not message.reply_to_message.from_user:
         return await message.reply_text(
-            "I can't change admin title of an unknown entity"
+            "Tôi không thể thay đổi tiêu đề quản trị viên của một thực thể không xác định"
         )
     chat_id = message.chat.id
     from_user = message.reply_to_message.from_user
     if len(message.command) < 2:
         return await message.reply_text(
-            "**Usage:**\n/set_user_title NEW ADMINISTRATOR TITLE"
+            "**Cách sử dụng:**\n/set_user_title CHỨC DANH QUẢN TRỊ VIÊN MỚI"
         )
     title = message.text.split(None, 1)[1]
     await app.set_administrator_title(chat_id, from_user.id, title)
@@ -73,26 +73,26 @@ async def set_user_title(_, message):
     )
 
 
-@app.on_message(filters.command("set_chat_photo") & ~filters.private)
+@app.on_message(filters.command("doianhnhom") & ~filters.private)
 @adminsOnly("can_change_info")
 async def set_chat_photo(_, message):
     reply = message.reply_to_message
 
     if not reply:
         return await message.reply_text(
-            "Reply to a photo to set it as chat_photo"
+            "Trả lời một bức ảnh để đặt nó thành ảnh đại diện nhóm"
         )
 
     file = reply.document or reply.photo
     if not file:
         return await message.reply_text(
-            "Reply to a photo or document to set it as chat_photo"
+            "Trả lời ảnh hoặc tài liệu để đặt nó thành ảnh đại diện nhóm"
         )
 
     if file.file_size > 5000000:
-        return await message.reply("File size too large.")
+        return await message.reply("Kích thước tập tin quá lớn.")
 
     photo = await reply.download()
     await message.chat.set_photo(photo)
-    await message.reply_text("Successfully Changed Group Photo")
+    await message.reply_text("Đã thay đổi ảnh nhóm thành công")
     os.remove(photo)
