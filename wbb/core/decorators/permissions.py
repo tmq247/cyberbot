484525f2fb7400fe1e5da26fdf80272b68b701a9
tmq247 <60788746+tmq247@users.noreply.github.com>
2025-04-09 00:49:06 +0700

    Update permissions.py

--- wbb/core/decorators/permissions.py
+++ wbb/core/decorators/permissions.py
@@ -50,8 +50,8 @@ async def authorised(func, subFunc2, client, message, *args, **kwargs):
 async def unauthorised(message: Message, permission, subFunc2):
     chatID = message.chat.id
     text = (
-        "You don't have the required permission to perform this action."
-        + f"\n**Permission:** __{permission}__"
+        "Bạn không có đủ quyền để thực hiện hành động này."
+        + f"\n**Quyền hạn:** __{permission}__"
     )
     try:
         await message.reply_text(text)
