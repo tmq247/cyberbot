21b6e26d0011a6fea737a8a411d034dfb69a0522
tmq247 <60788746+tmq247@users.noreply.github.com>
2025-05-07 17:22:51 +0700

    Update topchat.py

--- wbb/modules/topchat.py
+++ wbb/modules/topchat.py
@@ -12,7 +12,6 @@ from pyrogram.types import *
 #mongo_client = MongoClient(MONGO_URL)
 mongo_client = MongoClient(MONGO_URL)
 db = mongo_client["telegram_stats"]
-print(db)
 collection = db["message_counts"]
 
 
@@ -39,22 +38,22 @@ def track_messages(client, message):
 
 @app.on_message(filters.command("top", prefixes=["/", "!"]) & filters.group)
 async def send_top10(client, message):
+    
+    """ Gửi danh sách top 10 người nhắn nhiều nhất khi có lệnh /top10 trong nhóm hiện tại """
+    chat_id = message.chat.id
+    now = datetime.now()
+
+    top_weekly = collection.find({"chat_id": chat_id}).sort("weekly_count", -1).limit(10)
+    top_monthly = collection.find({"chat_id": chat_id}).sort("monthly_count", -1).limit(10)
+    if not top_weekly or not top_monthly:
+        await message.reply("Không có dữ liệu tin nhắn trong tuần hoặc tháng này.")
+        return
+
+    message_text = "🏆 **Top 10 người nhắn nhiều nhất:**\n\n"
+    message_text += "**📅 Trong tuần:**\n" + "\n".join([f"- [{user['user_id']}](tg://user?id={user['user_id']}): {user['weekly_count']} tin nhắn" for user in top_weekly])
+    message_text += "\n\n**🗓 Trong tháng:**\n" + "\n".join([f"- [{user['user_id']}](tg://user?id={user['user_id']}): {user['monthly_count']} tin nhắn" for user in top_monthly])
     try:
-        """ Gửi danh sách top 10 người nhắn nhiều nhất khi có lệnh /top10 trong nhóm hiện tại """
-        chat_id = message.chat.id
-        now = datetime.now()
-    
-        top_weekly = collection.find({"chat_id": chat_id}).sort("weekly_count", -1).limit(10)
-        top_monthly = collection.find({"chat_id": chat_id}).sort("monthly_count", -1).limit(10)
-        if not top_weekly or not top_monthly:
-            await message.reply("Không có dữ liệu tin nhắn trong tuần hoặc tháng này.")
-            return
-    
-        message_text = "🏆 **Top 10 người nhắn nhiều nhất:**\n\n"
-        message_text += "**📅 Trong tuần:**\n" + "\n".join([f"- [{user['user_id']}](tg://user?id={user['user_id']}): {user['weekly_count']} tin nhắn" for user in top_weekly])
-        message_text += "\n\n**🗓 Trong tháng:**\n" + "\n".join([f"- [{user['user_id']}](tg://user?id={user['user_id']}): {user['monthly_count']} tin nhắn" for user in top_monthly])
-    
-        await await message.reply(message_text, disable_web_page_preview=True)
+        await await app.send_message(message.chat.id, message_text, disable_web_page_preview=True)
     except Exception as e:
         print("Lỗi:", e)
         traceback.print_exc()
